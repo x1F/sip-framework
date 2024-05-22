76f312c4491c29eba487286556ed2d3359a6b21b
Martin Buchheim <94110177+MartinBuchheim@users.noreply.github.com>
2024-05-22 09:38:55 +0200

    Improved query- and path-parameter mapping for REST (#258)
    * Added query/path parameter mapping processors for RestInboundConnectorBase
    
    * Added tests for REST parameter mapping
    
    * Improved usage documentation
    
    * Improved usage documentation
    
    * Added changelog
    
    ---------
    
    Co-authored-by: malte-westendorf-ikor <82526705+malte-westendorf-ikor@users.noreply.github.com>

---
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/annotation/rest/ParameterMapping.java
@@ -0,0 +1,59 @@
+package de.ikor.sip.foundation.core.declarative.annotation.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a method within a {@link
+ * de.ikor.sip.foundation.core.declarative.connector.RestInboundConnectorBase} that is used to map
+ * path- and query-parameters to the request-model.
+ *
+ * <p>To use, create a public method using this annotation within your implementation of the
+ * connector class. You can use any number of parameters for that method, and the framework will
+ * automatically assign the corresponding data using a best-effort transformation the declared type.
+ * The following rules apply:
+ *
+ * <ul>
+ *   <li>Parameters annotated with @{@link PathParameter} will be filled with the respectively named
+ *       path parameter
+ *   <li>Parameters annotated with @{@link QueryParameter} will be filled with the respectively
+ *       named query parameter
+ *   <li>Parameters of type {@link org.apache.camel.Exchange} will receive the exchange
+ *   <li>Parameters of type {@link org.apache.camel.Message} will receive the message
+ *   <li>Parameters of any other type will receive the message body in the declared type
+ * </ul>
+ *
+ * If the provided data from the request can not be converted to the declared parameter type, the
+ * REST request will return a server error.
+ *
+ * <p>Example:
+ *
+ * <pre>{@code
+ * @InboundConnector
+ * public class RestApi extends RestInboundConnectorBase {
+ *
+ *     @Override
+ *     protected void configureRest(final RestDefinition definition) {
+ *       definition
+ *           .post("/api/{first}/{second}")
+ *           .type(ApiModel.class);
+ *     }
+ *
+ *     @ParameterMapping
+ *     public void mapParameters(ApiModel model, @PathParameter("first") Integer pathFirst, @PathParameter("second") String pathSecond) {
+ *          model.setFirst(pathFirst);
+ *          model.setSecond(pathSecond);
+ *     }
+ * }
+ *
+ * }</pre>
+ *
+ * @see PathParameter
+ * @see QueryParameter
+ * @see de.ikor.sip.foundation.core.declarative.connector.RestInboundConnectorBase
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ParameterMapping {}
---
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/annotation/rest/QueryParameter.java
@@ -0,0 +1,23 @@
+package de.ikor.sip.foundation.core.declarative.annotation.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates a REST query-parameter to be assigned to the annotated parameter of a mapping-function
+ * annotated with @{@link ParameterMapping}
+ *
+ * @see ParameterMapping
+ * @see PathParameter
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface QueryParameter {
+
+  /**
+   * @return Name of the query-parameter
+   */
+  String value();
+}
