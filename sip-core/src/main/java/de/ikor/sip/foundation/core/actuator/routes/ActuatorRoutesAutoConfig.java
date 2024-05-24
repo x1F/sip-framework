81082a031e9d40d6363fb2e5a21a595e4c67ec39
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.de>
2024-05-24 11:40:13 +0200

    Change adapter-routes endpoint to adapterroutes (#260)
    * Change adapter-routes endpoint to adapterroutes
    
    * Add changelog and docs

--- sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/routes/ActuatorRoutesAutoConfig.java
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/actuator/routes/ActuatorRoutesAutoConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 
 /** In charge of scanning all beans under de.ikor.sip.foundation.core.actuator.routes package. */
-@SIPFeature(name = FoundationFeature.ADAPTER_ROUTES, versions = 1)
+@SIPFeature(name = FoundationFeature.ADAPTER_ROUTES, versions = 2)
 @AutoConfiguration
 @ComponentScan
 @ConditionalOnProperty(value = "sip.core.actuator.adapter-routes.enabled", havingValue = "true")
