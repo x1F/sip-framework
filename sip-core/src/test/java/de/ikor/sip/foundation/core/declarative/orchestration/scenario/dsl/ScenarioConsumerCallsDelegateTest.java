c8bde7bbd724368d71ebdfea3593ae2dc0972ee9
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.one>
2024-10-07 17:10:19 +0200

    Add changelog and test

---
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/declarative/orchestration/scenario/dsl/ScenarioConsumerCallsDelegateTest.java
@@ -0,0 +1,29 @@
+package de.ikor.sip.foundation.core.declarative.orchestration.scenario.dsl;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+import de.ikor.sip.foundation.core.declarative.annonation.IntegrationScenario;
+import de.ikor.sip.foundation.core.declarative.connector.GenericOutboundConnectorBase;
+import de.ikor.sip.foundation.core.declarative.scenario.IntegrationScenarioBase;
+import de.ikor.sip.foundation.core.util.exception.SIPFrameworkInitializationException;
+import org.junit.jupiter.api.Test;
+
+class ScenarioConsumerCallsDelegateTest {
+
+  @IntegrationScenario(scenarioId = "test", requestModel = String.class)
+  class TestScenario extends IntegrationScenarioBase {}
+
+  @Test
+  void
+      When_NoResponseInScenario_With_ResponseHandlerInOrchestration_Then_ThrowInitializationException() {
+    var s =
+        new CallScenarioConsumerBaseNoResponseDefinition(
+            mock(), new TestScenario(), GenericOutboundConnectorBase.class);
+
+    assertThatThrownBy(() -> s.andHandleResponse((a, b) -> {}))
+        .isInstanceOf(SIPFrameworkInitializationException.class)
+        .hasMessageContaining(
+            "Integration Scenario test does not have a response model defined, using response handler is not intended.");
+  }
+}
