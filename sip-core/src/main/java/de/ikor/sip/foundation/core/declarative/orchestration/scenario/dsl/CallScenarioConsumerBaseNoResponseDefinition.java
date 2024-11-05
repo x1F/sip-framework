b162b901aa46e8868b451d944cb1c95965ba9adf
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.de>
2024-11-05 12:34:23 +0100

    Throw exception when response handler is used in no response scenario (#276)
    Throw exception if response handler is used in scenario orchestration with no response model:
    
    ---------
    
    Co-authored-by: Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.one>

---
+++ sip-core/src/main/java/de/ikor/sip/foundation/core/declarative/orchestration/scenario/dsl/CallScenarioConsumerBaseNoResponseDefinition.java
@@ -0,0 +1,33 @@
+package de.ikor.sip.foundation.core.declarative.orchestration.scenario.dsl;
+
+import de.ikor.sip.foundation.core.declarative.scenario.IntegrationScenarioConsumerDefinition;
+import de.ikor.sip.foundation.core.declarative.scenario.IntegrationScenarioDefinition;
+import de.ikor.sip.foundation.core.util.exception.SIPFrameworkInitializationException;
+
+/**
+ * DSL class for calling a scenario consumer specified by it's class for scenarios without response
+ */
+public final class CallScenarioConsumerBaseNoResponseDefinition<R, M>
+    extends CallScenarioConsumerByClassDefinition<R, M> {
+
+  CallScenarioConsumerBaseNoResponseDefinition(
+      R dslReturnDefinition,
+      IntegrationScenarioDefinition integrationScenario,
+      Class<? extends IntegrationScenarioConsumerDefinition> consumerClass) {
+    super(dslReturnDefinition, integrationScenario, consumerClass);
+  }
+
+  @Override
+  public R andHandleResponse(ScenarioStepResponseConsumer<M> responseConsumer) {
+    throw SIPFrameworkInitializationException.init(
+        "Integration Scenario %s does not have a response model defined, using response handler is not intended.",
+        getIntegrationScenario().getId());
+  }
+
+  @Override
+  public R andAggregateResponse(final ScenarioStepResponseAggregator<M> responseAggregator) {
+    throw SIPFrameworkInitializationException.init(
+        "Integration Scenario %s does not have a response model defined, using aggregate response is not intended.",
+        getIntegrationScenario().getId());
+  }
+}
