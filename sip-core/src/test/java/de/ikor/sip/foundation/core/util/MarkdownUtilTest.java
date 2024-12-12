dd1bc90e91db960d4bf4e6e3210d5bd4e415487c
Martin Buchheim <martin.buchheim@ikor.one>
2024-12-12 09:15:06 +0100

    Added utility to embed local image resources as data streams into Markdown files

---
+++ sip-core/src/test/java/de/ikor/sip/foundation/core/util/MarkdownUtilTest.java
@@ -0,0 +1,23 @@
+package de.ikor.sip.foundation.core.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MarkdownUtilTest {
+
+    @Test
+    public void GIVEN_classpath_markdown_WHEN_embedding_image_files_VERIFY_result_contains_embedded_image() throws IOException {
+        final var markdownString = MarkdownUtil.readMarkdownFileAndEmbedLocalImages(new ClassPathResource("markdown/markdown-embed.md"));
+
+        assertThat(markdownString)
+                .isNotBlank()
+                .contains("Karte_Odenwaldexpress.png", "/this/file/does/not/exist.jpg")
+                .doesNotContain("OdenwaldexpressMudau.jpg");
+    }
+
+
+}
