e80a18e7766a4e900924f36688a46a5ddad599c1
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.one>
2025-10-15 11:42:10 +0200

    Update message and define annotation-parent class pairs

--- sip-annotation-processor/src/main/java/one/x1f/sip/foundation/SIPAnnotationProcessor.java
+++ sip-annotation-processor/src/main/java/one/x1f/sip/foundation/SIPAnnotationProcessor.java
@@ -1,45 +1,95 @@
 package one.x1f.sip.foundation;
 
-import javax.annotation.processing.*;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.*;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
+import java.util.Map;
 import java.util.Set;
 
-@SupportedAnnotationTypes("one.x1f.sip.foundation.core.declarative.annotation.InboundConnector")
+@SupportedAnnotationTypes("one.x1f.sip.foundation.core.declarative.annotation.*")
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 public class SIPAnnotationProcessor extends AbstractProcessor {
+
+    private final Map<String, String> expectedParentClasses = Map.of(
+            "one.x1f.sip.foundation.core.declarative.annotation.InboundConnector", "one.x1f.sip.foundation.core.declarative.connector.InboundConnectorBase",
+            "one.x1f.sip.foundation.core.declarative.annotation.OutboundConnector", "one.x1f.sip.foundation.core.declarative.connector.GenericOutboundConnectorBase",
+            "one.x1f.sip.foundation.core.declarative.annotation.IntegrationScenario", "one.x1f.sip.foundation.core.declarative.scenario.IntegrationScenarioBase",
+            "one.x1f.sip.foundation.core.declarative.annotation.CompositeProcess", "one.x1f.sip.foundation.core.declarative.process.CompositeProcessBase",
+            "one.x1f.sip.foundation.core.declarative.annotation.ConnectorGroup", "one.x1f.sip.foundation.core.declarative.connectorgroup.ConnectorGroupBase"
+    );
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (roundEnv.processingOver()) {
             return false;
         }
+
+        for (TypeElement annotation : annotations) {
+            validateAnnotatedClassParent(roundEnv, annotation);
+        }
+        return false;
+    }
+
+    private void validateAnnotatedClassParent(RoundEnvironment roundEnv, TypeElement annotation) {
+        String annotationName = annotation.getQualifiedName().toString();
+        String expectedParent = expectedParentClasses.get(annotationName);
+        if (expectedParent == null) return;
+
+        for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
+            if (element.getKind() != ElementKind.CLASS) continue;
+
+            TypeElement classElement = (TypeElement) element;
+            if (element.getKind() == ElementKind.CLASS) {
+
+                if (!extendsClass(classElement, expectedParent)) {
+                    processingEnv.getMessager().printMessage(
+                            Diagnostic.Kind.ERROR,
+                            String.format("Class %s annotated with @%s must extend %s",
+                                    classElement.getQualifiedName(),
+                                    annotation.getSimpleName(),
+                                    expectedParent),
+                            element
+                    );
+                } else {
+                    processingEnv.getMessager().printMessage(
+                            Diagnostic.Kind.NOTE,
+                            String.format("Class %s annotated with @%s extends %s",
+                                    classElement.getQualifiedName(),
+                                    annotation.getSimpleName(),
+                                    expectedParent),
+                            element
+                    );
+                }
+
+            }
+        }
+    }
+
+    private boolean extendsClass(TypeElement type, String expectedParent) {
         Types typeUtils = processingEnv.getTypeUtils();
         Elements elementUtils = processingEnv.getElementUtils();
-        for (TypeElement annotation : annotations) {
-            TypeElement parentClass = elementUtils.getTypeElement("one.x1f.sip.foundation.core.declarative.connector.InboundConnectorBase");
-            for (Element element : roundEnv.getElementsAnnotatedWith(annotation)) {
-                if (element.getKind() == ElementKind.CLASS) {
-                    TypeMirror classType = element.asType();
+        TypeElement expectedParentElement = elementUtils.getTypeElement(expectedParent);
+        if (expectedParentElement == null) return false;
 
-                    if (typeUtils.isSubtype(classType, parentClass.asType())) {
-                        processingEnv.getMessager().printMessage(
-                                Diagnostic.Kind.NOTE,
-                                element.getSimpleName() + " extends InboundConnectorBase"
-                        );
-                    } else {
-                        processingEnv.getMessager().printMessage(
-                                Diagnostic.Kind.ERROR,
-                                element.getSimpleName() + " must extend InboundConnectorBase"
-                        );
-                    }
+        TypeMirror expectedMirror = expectedParentElement.asType();
+        TypeMirror current = type.getSuperclass();
 
-                }
+        while (current.getKind() != TypeKind.NONE) {
+            if (typeUtils.isSameType(current, expectedMirror)) {
+                return true;
             }
+            TypeElement parent = (TypeElement) typeUtils.asElement(current);
+            current = parent.getSuperclass();
         }
         return false;
     }
