72df72cba05c89b3e5760e7f87f722a6a06d82ce
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.one>
2025-12-12 13:15:02 +0100

    Prevent NPE when using REST connector with empty path value in HTTP method

--- sip-core/src/main/java/one/x1f/sip/foundation/core/openapi/OpenApiContextPathResolver.java
+++ sip-core/src/main/java/one/x1f/sip/foundation/core/openapi/OpenApiContextPathResolver.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.Paths;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.apache.camel.CamelContext;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -26,7 +27,9 @@ public class OpenApiContextPathResolver {
     if (openAPICamelConfig.getPaths() != null && contextPath != null) {
       openAPICamelConfig
           .getPaths()
-          .forEach((path, pathItem) -> paths.put(contextPath.concat(path), pathItem));
+          .forEach(
+              (path, pathItem) ->
+                  paths.put(contextPath.concat(StringUtils.isNotEmpty(path) ? path : ""), pathItem));
       openAPICamelConfig.setPaths(paths);
     }
   }
