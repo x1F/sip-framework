6940adadd4e441ae974a71414d538b8a4b908f67
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.one>
2026-04-17 12:27:20 +0200

    change property errorHandler to _SipErrorHandler

--- sip-core/src/main/java/one/x1f/sip/foundation/core/declarative/utils/DeclarativeHelper.java
+++ sip-core/src/main/java/one/x1f/sip/foundation/core/declarative/utils/DeclarativeHelper.java
@@ -38,7 +38,7 @@ import org.springframework.context.ApplicationContext;
 public class DeclarativeHelper {
 
   public static final String CONNECTOR_ID_FORMAT = "%s-%s-%s";
-  public static final String ERROR_HANDLER = "errorHandler";
+  public static final String ERROR_HANDLER = "_SipErrorHandler";
   public static final String SIP_INTERNAL_SET_PROPERTY = "sip-internal-set-property";
   private static final Pattern DOLLAR_PLACEHOLDER_PATTERN = Pattern.compile("\\$\\{[^}]+\\}");
   private static final Pattern DOUBLE_CURLY_PLACEHOLDER_PATTERN =
