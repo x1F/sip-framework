a2f4faa41ea896ca77bf79b498abb05b3bde2a09
Nemanja Nedeljkovic <nemanja.nedeljkovic@ikor.one>
2026-07-31 13:56:02 +0200

    Fix javadoc

--- sip-core/src/main/java/one/x1f/sip/foundation/core/declarative/orchestration/process/CompositeProcessTransformer.java
+++ sip-core/src/main/java/one/x1f/sip/foundation/core/declarative/orchestration/process/CompositeProcessTransformer.java
@@ -1,14 +1,13 @@
 package one.x1f.sip.foundation.core.declarative.orchestration.process;
 
-/** Interface to extract the request object for a process consumer call */
+/** Interface to expose orchestration context in process step */
 @FunctionalInterface
 public interface CompositeProcessTransformer {
 
   /**
-   * Returns the request object to be used with the consumer call
+   * Define processing on orchestration context
    *
    * @param context The current orchestration context
-   * @return Request sent to the process consumer
    */
   void process(final CompositeProcessOrchestrationContext context);
 }
