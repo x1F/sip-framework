b6e6aafddb3f72ac3333a337bb729ad532ef92f8
Malte Westendorf <malte.westendorf@x1f.one>
2026-07-31 17:57:27 +0200

    Introduced Composite Tracer and two new implementation of TraceSupport for extended Logging.

---
+++ sip-core/src/main/java/one/x1f/sip/foundation/core/trace/CompositeTracer.java
@@ -0,0 +1,137 @@
+package one.x1f.sip.foundation.core.trace;
+
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.NamedNode;
+import org.apache.camel.NamedRoute;
+import org.apache.camel.spi.ExchangeFormatter;
+import org.apache.camel.spi.Tracer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Primary
+@ConditionalOnBooleanProperty(value = "sip.core.tracing.enabled")
+@Component
+public class CompositeTracer implements Tracer {
+
+  private final List<TraceSupport> tracers;
+
+  /**
+   * Creates new instance of CompositeTracer
+   *
+   * @param tracers list of {@link TraceSupport} available as beans
+   */
+  public CompositeTracer(List<TraceSupport> tracers) {
+    this.tracers = tracers;
+  }
+
+  @Override
+  public boolean shouldTrace(NamedNode definition) {
+    return true;
+  }
+
+  @Override
+  public void traceBeforeRoute(NamedRoute route, Exchange exchange) {
+    filteredTracers(null).forEach(tracer -> tracer.traceBeforeRoute(route, exchange));
+  }
+
+  @Override
+  public void traceBeforeNode(NamedNode node, Exchange exchange) {
+    filteredTracers(node).forEach(tracer -> tracer.traceBeforeNode(node, exchange));
+  }
+
+  @Override
+  public void traceAfterNode(NamedNode node, Exchange exchange) {
+    filteredTracers(node).forEach(tracer -> tracer.traceAfterNode(node, exchange));
+  }
+
+  @Override
+  public void traceSentNode(NamedNode node, Exchange exchange, Endpoint endpoint, long elapsed) {
+    filteredTracers(node)
+        .forEach(tracer -> tracer.traceSentNode(node, exchange, endpoint, elapsed));
+  }
+
+  @Override
+  public void traceAfterRoute(NamedRoute route, Exchange exchange) {
+    filteredTracers(null).forEach(tracer -> tracer.traceAfterRoute(route, exchange));
+  }
+
+  @Override
+  public long getTraceCounter() {
+    return 0;
+  }
+
+  @Override
+  public void resetTraceCounter() {}
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public void setEnabled(boolean enabled) {}
+
+  @Override
+  public boolean isStandby() {
+    return false;
+  }
+
+  @Override
+  public void setStandby(boolean standby) {}
+
+  @Override
+  public boolean isTraceRests() {
+    return false;
+  }
+
+  @Override
+  public void setTraceRests(boolean traceRests) {}
+
+  @Override
+  public boolean isTraceTemplates() {
+    return false;
+  }
+
+  @Override
+  public void setTraceTemplates(boolean traceTemplates) {}
+
+  @Override
+  public String getTracePattern() {
+    return "";
+  }
+
+  @Override
+  public void setTracePattern(String tracePattern) {}
+
+  @Override
+  public boolean isTraceBeforeAndAfterRoute() {
+    return false;
+  }
+
+  @Override
+  public void setTraceBeforeAndAfterRoute(boolean traceBeforeAndAfterRoute) {}
+
+  @Override
+  public ExchangeFormatter getExchangeFormatter() {
+    return null;
+  }
+
+  @Override
+  public void setExchangeFormatter(ExchangeFormatter exchangeFormatter) {}
+
+  @Override
+  public void start() {}
+
+  @Override
+  public void stop() {}
+
+  private Stream<TraceSupport> filteredTracers(NamedNode node) {
+    return tracers.stream().filter(tracer -> tracer.shouldTrace(node));
+  }
+}
